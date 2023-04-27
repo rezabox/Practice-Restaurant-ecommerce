b0c2fb6b61bc8245c5a8ad7974f5c6649a774c2d
rezabox <reza.asareh81@gmail.com>
2023-04-28 00:08:04 +0430

    add navbar_toggle

--- src/Components/Navbar/Header.js
+++ src/Components/Navbar/Header.js
@@ -8,18 +8,30 @@ function Header() {
   const [navbar,setNavbar] = useState(false);
   const [link,setLink] = useState(false);
   const [arrowTop,setArrowTop] = useState(false);
+  const [menuToggle,setMenuToggle] = useState(false);
+  const [showMenu,setShowMenu] = useState(false);
   const changeColorNavbar = ()=>{
    if(window.scrollY >= 80){
         setNavbar(true);
         setLink(true)
         setArrowTop(true);
+        setMenuToggle(true);
    }else{
         setNavbar(false);
         setLink(false);
         setArrowTop(false);
+        setMenuToggle(false);
    }
   }  
   window.addEventListener('scroll', changeColorNavbar);
+  const menuShow = ()=>{
+    if(!showMenu){
+      setShowMenu(true); 
+    }else{
+      setShowMenu(false);
+    }
+  }
+
 
   return (
     <nav>
@@ -28,13 +40,16 @@ function Header() {
                   <div className='navbar-header'></div>
                   <div className={styles.navbar}>
                     <div className='right_nav'>
-                    <ul id='smooth-scroll' className={styles.navbar_nav}>
+                    <ul id='smooth-scroll' className={showMenu ? 'navbar_nav active' : 'navbar_nav'}>
                        <li><a href="/" className={link ? 'link active' : ''}>صفحه اصلی</a></li>
                        <li><a href='/FoodOrder' className={link ? 'link active' : ''}>سفارش آنلاین</a></li>
                        <li><a href='/News' className={link ? 'link active' : ''}>اخبار</a></li>
                        <li><a href='/about' className={link ? 'link active' : ''}>درباره ما</a></li>
                        <li><a href='/map' className={link ? 'link active' : ''}>ارتباط با ما</a></li>
                     </ul>
+                    <button className={menuToggle ? 'toggle_menu active' : 'toggle_menu'} onClick={menuShow}>
+                    <i class="bi bi-justify"></i>
+                    </button>
                     </div>
                     <div className='left_nav'>
                        <li><a href="/login"  className={link ? 'link active' : ''} ><span><i className="bi bi-person-fill"></i></span>ورود/ثبتنام</a></li>     
