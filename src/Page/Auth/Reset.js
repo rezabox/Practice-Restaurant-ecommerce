9199a82dd33995f2651d54adccb1bf17a26ba14f
rezabox <reza.asareh81@gmail.com>
2023-05-21 11:39:14 +0330

    update resetjs

--- src/Page/Auth/Reset.js
+++ src/Page/Auth/Reset.js
@@ -1,21 +1,6 @@
 import React from 'react'
 import styles from './auth.module.scss';
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import icon1 from '../assets/eye-fill.svg';
-import icon2 from '../assets/eye-slash-fill.svg';
 function Reset() {
-  const [change,setChange] = useState('password');
-  const [icon,setIcon] = useState(icon2);
-  const handleClick = () => {
-     if(change === 'password'){
-        setChange('text');
-        setIcon(icon1);
-     }else{
-        setChange('password');
-        setIcon(icon2);
-     }
-  }
   return (
     <section className={styles.row}>
       <card className={styles.cards}>
