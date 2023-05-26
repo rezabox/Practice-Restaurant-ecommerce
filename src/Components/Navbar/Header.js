85a2f2677c6d8d27311d7b6b396e7f3c17ac9f8e
rezabox <reza.asareh81@gmail.com>
2023-05-26 20:21:20 +0330

    add Navlink to header

--- src/Components/Navbar/Header.js
+++ src/Components/Navbar/Header.js
@@ -1,70 +1,102 @@
-import React, { useState } from 'react'
-// import {NavLink} from 'react-router-dom';
-import styles from './Header.module.scss'
-import './Header.scss';
+import React, { useState } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import styles from "./Header.module.scss";
+import "./Header.scss";
 const activeLink = ({ isActive }) => (isActive ? `${styles.active}` : "");
 
-function Header() { 
-  const [navbar,setNavbar] = useState(false);
-  const [link,setLink] = useState(false);
-  const [arrowTop,setArrowTop] = useState(false);
-  const [menuToggle,setMenuToggle] = useState(false);
-  const [showMenu,setShowMenu] = useState(false);
-  const changeColorNavbar = ()=>{
-   if(window.scrollY >= 80){
-        setNavbar(true);
-        setLink(true)
-        setArrowTop(true);
-        setMenuToggle(true);
-   }else{
-        setNavbar(false);
-        setLink(false);
-        setArrowTop(false);
-        setMenuToggle(false);
-   }
-  }  
-  window.addEventListener('scroll', changeColorNavbar);
-  const menuShow = ()=>{
-    if(!showMenu){
-      setShowMenu(true); 
-    }else{
+function Header() {
+  const [navbar, setNavbar] = useState(false);
+  const [link, setLink] = useState(false);
+  const [arrowTop, setArrowTop] = useState(false);
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const changeColorNavbar = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+      setLink(true);
+      setArrowTop(true);
+      setMenuToggle(true);
+    } else {
+      setNavbar(false);
+      setLink(false);
+      setArrowTop(false);
+      setMenuToggle(false);
+    }
+  };
+  window.addEventListener("scroll", changeColorNavbar);
+  const menuShow = () => {
+    if (!showMenu) {
+      setShowMenu(true);
+    } else {
       setShowMenu(false);
     }
-  }
-
+  };
 
   return (
     <nav>
-       <div className='container'>
-             <div className={navbar ? 'navbar active' : 'navbar'}>
-                  <div className='navbar-header'></div>
-                  <div className={styles.navbar}>
-                    <div className='right_nav'>
-                    <ul id='smooth-scroll' className={showMenu ? 'navbar_nav active' : 'navbar_nav'}>
-                       <li><a href="/" className={link ? 'link active' : ''}>صفحه اصلی</a></li>
-                       <li><a href='/FoodOrder' className={link ? 'link active' : ''}>سفارش آنلاین</a></li>
-                       <li><a href='/News' className={link ? 'link active' : ''}>اخبار</a></li>
-                       <li><a href='/about' className={link ? 'link active' : ''}>درباره ما</a></li>
-                       <li><a href='/connect' className={link ? 'link active' : ''}>ارتباط با ما</a></li>
-                    </ul>
-                    <button className={menuToggle ? 'toggle_menu active' : 'toggle_menu'} onClick={menuShow}>
-                    <i class="bi bi-justify"></i>
-                    </button>
-                    </div>
-                    <div className='left_nav'>
-                       <li><a href="/login"  className={link ? 'link active' : ''} ><span><i className="bi bi-person-fill"></i></span>ورود/ثبتنام</a></li>     
-                    </div>
-               </div>
-             </div>
-          </div>  
-          <div className={arrowTop ? 'arrow2 active' : 'arrow2'}>
-              <p id="back-up" className='arrow_top'>
-                 <a href='#'>
-                 <i class="bi bi-chevron-up"></i>
-                 </a>
-              </p>
-          </div>     
+      <div className="container">
+        <div className={navbar ? "navbar active" : "navbar"}>
+          <div className="navbar-header"></div>
+          <div className={styles.navbar}>
+            <div className="right_nav">
+              <ul
+                id="smooth-scroll"
+                className={showMenu ? "navbar_nav active" : "navbar_nav"}
+              >
+                <li>
+                  <NavLink to="/" className={link ? "link active" : ""} onClick={activeLink}>
+                    صفحه اصلی
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/FoodOrder" className={link ? "link active" : ""} onClick={activeLink}>
+                    سفارش آنلاین
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/News" className={link ? "link active" : ""} onClick={activeLink}>
+                    اخبار
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about" className={link ? "link active" : ""} onClick={activeLink}>
+                    درباره ما
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/connect" className={link ? "link active" : ""} onClick={activeLink}>
+                    ارتباط با ما
+                  </NavLink>
+                </li>
+              </ul>
+              <button
+                className={menuToggle ? "toggle_menu active" : "toggle_menu"}
+                onClick={menuShow}
+              >
+                <i class="bi bi-justify"></i>
+              </button>
+            </div>
+            <div className="left_nav">
+                <NavLink to="/login" className={link ? "link active" : ""} onClick={activeLink}>
+                  <span>
+                    <i className="bi bi-person-fill"></i>
+                  </span>
+                  ورود/ثبتنام
+                </NavLink>
+              
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={arrowTop ? "arrow2 active" : "arrow2"}>
+        <p id="back-up" className="arrow_top">
+          <a href="#">
+            <i class="bi bi-chevron-up"></i>
+          </a>
+        </p>
+      </div>
     </nav>
-  )
+  );
 }
-export default Header
+export default Header;
